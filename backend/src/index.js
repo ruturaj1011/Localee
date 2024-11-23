8fdbb375a9de2565808bb86e6654a6fb811c843f
ruturaj1011 <ruturajnshinde1011@gmail.com>
2024-11-23 14:58:26 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from "express";
+import {createServer} from "node:http";
+
+import mongoose from "mongoose";
+import cors from "cors";
+
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+const server = createServer(app);
+
+app.use(cors());
+app.use(express.json({limit : "40kb"}));
+app.use(express.urlencoded({limit:"40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const PORT = 8000;
+const MONGO_URL = process.env.MONGO_URL;
+
+const start = async () => {
+
+    const connectionDb = await mongoose.connect(MONGO_URL);
+    console.log(`MONGO Connected DB Host : ${connectionDb.connection.host}`);
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+}
+
+start();
---
+++ frontend/src/profilePanels/Vendor/AddNewService.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AddNewService() {
+    return ( 
+        <div>Add Service</div>
+     );
+}
+
+export default AddNewService;
